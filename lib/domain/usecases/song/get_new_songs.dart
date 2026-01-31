96851361625778f29b814b6f4543c39c266e357d
Aman Singh <amansingh08087@gmail.com>
2026-02-01 04:43:18 +0530

    news_songs

--- lib/domain/usecases/song/get_new_songs.dart
+++ lib/domain/usecases/song/get_new_songs.dart
@@ -1,12 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_two/core/usecase.dart';
 import 'package:flutter_two/data/repository/song/song_repository_impl.dart';
+import 'package:flutter_two/domain/repository/song/song.dart';
 
 import '../../../service_locator.dart';
 
 class GetNewSongsUseCase implements UseCase<Either,dynamic>{
   @override
   Future<Either> call(params) async{
-    return await sl<SongRepositoryImpl>().getNewSong();
+    return await sl<SongRepository>().getNewSong();
   }
 }
--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_two/core/configs/theme/app_colors.dart';
 import 'package:flutter_two/domain/entities/song/song.dart';
 import 'package:flutter_two/presentation/home/bloc/news_songs_cubit.dart';
 import 'package:flutter_two/presentation/home/bloc/news_songs_state.dart';
@@ -40,6 +41,7 @@ class NewsSongs extends StatelessWidget {
         return SizedBox(
           width: 160,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 child: Container(
@@ -47,8 +49,37 @@ class NewsSongs extends StatelessWidget {
                     borderRadius: BorderRadius.circular(30),
                     image: DecorationImage(image: NetworkImage(AppUrls.fireStorage),fit: BoxFit.cover)
                   ),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      transform: Matrix4.translationValues(10,10,0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.darkGrey
+                      ),
+                      child: Icon(Icons.play_arrow_rounded,color: Color(0xff959595),),
+                    ),
+                  ),
                 ),
-              )
+              ),
+              SizedBox(height: 10,),
+              Text(
+                songs[index].title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16
+                ),
+              ),
+              SizedBox(height: 5,),
+              Text(
+                songs[index].artist,
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12
+                ),
+              ),
             ],
           ),
         );
