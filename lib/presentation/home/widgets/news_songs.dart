4b079adcaa6751775a98a9795ca4d0afa125c9bb
Aman Singh <amansingh08087@gmail.com>
2026-02-02 15:33:41 +0530

    done

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -6,6 +6,7 @@ import 'package:flutter_two/presentation/home/bloc/news_songs_cubit.dart';
 import 'package:flutter_two/presentation/home/bloc/news_songs_state.dart';
 
 import '../../../core/configs/constants/app_urls.dart';
+import '../../song_player/pages/song_player.dart';
 
 class NewsSongs extends StatelessWidget {
   const NewsSongs({super.key});
@@ -19,13 +20,14 @@ class NewsSongs extends StatelessWidget {
         child: BlocBuilder<NewsSongsCubit, NewsSongsState>(
           builder: (context, state) {
             if (state is NewsSongsLoading) {
-              return Container(alignment:Alignment.center,child: CircularProgressIndicator());
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
             }
 
             if (state is NewsSongsLoaded) {
-              return _songs(
-                state.songs
-              );
+              return _songs(state.songs);
             }
             return Container();
           },
@@ -38,49 +40,59 @@ class NewsSongs extends StatelessWidget {
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        return SizedBox(
-          width: 160,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    image: DecorationImage(image: NetworkImage(AppUrls.fireStorage),fit: BoxFit.cover)
-                  ),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      height: 40,
-                      width: 40,
-                      transform: Matrix4.translationValues(10,10,0),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: AppColors.darkGrey
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => SongPlayerPage(),
+              ),
+            );
+          },
+          child: SizedBox(
+            width: 160,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: DecorationImage(
+                        image: NetworkImage(AppUrls.fireStorage),
+                        fit: BoxFit.cover,
                       ),
-                      child: Icon(Icons.play_arrow_rounded,color: Color(0xff959595),),
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        transform: Matrix4.translationValues(10, 10, 0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.darkGrey,
+                        ),
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                          color: Color(0xff959595),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              Text(
-                songs[index].title,
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16
+                SizedBox(height: 10),
+                Text(
+                  songs[index].title,
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
                 ),
-              ),
-              SizedBox(height: 5,),
-              Text(
-                songs[index].artist,
-                style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 12  
+                SizedBox(height: 5),
+                Text(
+                  songs[index].artist,
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
--- lib/presentation/home/widgets/play_list.dart
+++ lib/presentation/home/widgets/play_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_two/common/helpers/is_dark_mode.dart';
 import 'package:flutter_two/presentation/home/bloc/playlist_cubit.dart';
 import 'package:flutter_two/presentation/home/bloc/playlist_state.dart';
+import 'package:flutter_two/presentation/song_player/pages/song_player.dart';
 
 import '../../../core/configs/theme/app_colors.dart';
 import '../../../domain/entities/song/song.dart';
@@ -68,51 +69,61 @@ class PlayList extends StatelessWidget {
       separatorBuilder: (context, index) => const SizedBox(height: 10),
       itemBuilder: (context, index) {
         final song = songs[index];
-        return Row(
-          children: [
-            Container(
-              height: 45,
-              width: 45,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: context.isDarkMode
-                    ? AppColors.darkGrey
-                    : const Color(0xffE6E6E6),
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => SongPlayerPage(),
               ),
-              child: Icon(
-                Icons.play_arrow_rounded,
-                color: context.isDarkMode
-                    ? Color(0xff959595)
-                    : Color(0xff555555),
+            );
+          },
+          child: Row(
+            children: [
+              Container(
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: context.isDarkMode
+                      ? AppColors.darkGrey
+                      : const Color(0xffE6E6E6),
+                ),
+                child: Icon(
+                  Icons.play_arrow_rounded,
+                  color: context.isDarkMode
+                      ? Color(0xff959595)
+                      : Color(0xff555555),
+                ),
               ),
-            ),
-            const SizedBox(width: 12),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  song.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  song.artist,
-                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 11),
-                ),
-              ],
-            ),
-            Spacer(),
-            Row(
-              children: [
-                Text(song.duration.toString().replaceAll(".", ":")),
-                SizedBox(width: 20),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.favorite_rounded, color: AppColors.grey),
-                ),
-              ],
-            ),
-          ],
+              const SizedBox(width: 12),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    song.title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    song.artist,
+                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 11),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Text(song.duration.toString().replaceAll(".", ":")),
+                  SizedBox(width: 20),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.favorite_rounded, color: AppColors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
         );
       },
     );
